f2ec7abcf4246ba4db511f962c4b7a9a2b623416
Raoul DIFFOUO <diraulo@gmail.com>
2018-01-29 16:45:08 +0200

    Update dorbell.js script for EN site (#7)

--- source/javascripts/doorbell_en.js
+++ source/javascripts/doorbell_en.js
@@ -1,6 +1,10 @@
 window.doorbellOptions = {
+  id: '1974',
   appKey: 'zF0anPyXM2iItB1yfkqhwe8iMBdR4mBuiKPuwH2oe0OuFBvK9IFoksmZVigjgaGS'
 };
-(function(d, t) {
-  var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/3039?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g);
-}(document, 'script'));
+(function(w, d, t) {
+  var hasLoaded = false;
+  function l() { if (hasLoaded) { return; } hasLoaded = true; window.doorbellOptions.windowLoaded = true; var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/'+window.doorbellOptions['id']+'?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g); }
+  if (w.attachEvent) { w.attachEvent('onload', l); } else if (w.addEventListener) { w.addEventListener('load', l, false); } else { l(); }
+  if (d.readyState == 'complete') { l(); }
+}(window, document, 'script'));
